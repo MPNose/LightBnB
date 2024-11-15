af1be992ec20b1072336ad2534cc2a5e34459b37
MPNose <mpnoseworthy89@gmail.com>
2024-11-14 19:21:00 -0800

    modified addProperty function to update database

--- LightBnB_WebApp/db/database.js
+++ LightBnB_WebApp/db/database.js
@@ -125,11 +125,11 @@ const getAllProperties = (options, limit = 10) => {
     queryString += `AND owner_id = $${queryParams.length}`;
   } 
   if(options.city) {
-    queryParams.push(`%${options.city}`);
+    queryParams.push(`%${options.city}%`);
     queryString += `AND city LIKE $${queryParams.length}`;
   }    
-  if(options.minumum_price_per_night) {
-    queryParams.push(options.minumum_price_per_night);
+  if(options.minimum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night);
     queryString += `AND (cost_per_night/100) >= $${queryParams.length}`;
   } 
   if(options.maximum_price_per_night) {
@@ -162,11 +162,24 @@ const getAllProperties = (options, limit = 10) => {
  * @param {{}} property An object containing all of the property details.
  * @return {Promise<{}>} A promise to the property.
  */
-const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+const addProperty = (property) => {
+  return pool
+    .query(`
+      INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, 
+      cover_photo_url, cost_per_night, street, city, province, post_code, country, 
+      parking_spaces, number_of_bathrooms, number_of_bedrooms)
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+      RETURNING*;
+      `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, 
+        property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, 
+        property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, 
+        property.number_of_bedrooms])
+    .then((result) => {
+      return result.rows;
+    })    
+    .catch((err) => {
+      console.log(err.message);
+    })
 };
 
 module.exports = {
